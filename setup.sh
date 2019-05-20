3518888d62f6d96113cae05f873af3f8cda68026
Manuel Burger <burgerm@student.ethz.ch>
2019-05-20 08:32:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+echo "Download and setup z command..."
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh
+cp z.sh ~/.z.sh
+
 echo "Moving files to home directory..."
 cp .bash_profile ~/
 cp .git-prompt.sh ~/
